9806829a29a6d92ba3c6f75d1cc9d663067def4f
danniddr1981 <153033115+danniddr1981@users.noreply.github.com>
2023-12-05 19:50:15 -0500

    Update Scene1.js

--- Scene1.js
+++ Scene1.js
@@ -2,7 +2,7 @@ class Scene1 extends Phaser.Scene {
 {
     constructor ()
     {
-        super();
+        super("BootGame");
     }
   create() {
         this.add.text(20, 20, 'Starting Game...', { color: '#00ff00' }).setOrigin(0.5, 0);
