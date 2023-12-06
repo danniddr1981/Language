43b608e46243563ac557000d1bc36f7e7f52a03f
danniddr1981 <153033115+danniddr1981@users.noreply.github.com>
2023-12-05 20:05:06 -0500

    Update Scene2.js

--- Scene2.js
+++ Scene2.js
@@ -2,4 +2,7 @@ class Scene2 extends Phaser.Scene {
 	constructor() {
 		super("playGame");
 	}
+	create () {
+        this.add.text(20, 20, "Playing Game", { color: "#00ff00", font: "25px Arial", fill: "Yellow" }).setOrigin(0.5, 0);
+	}
 }
